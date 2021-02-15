ad84518420549ead55015a1347c05d62f47b045c
Oliver Hughes <straightupdirtybass@gmail.com>
2021-02-15 17:11:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+def code():
+  print("\nSelect Operation")
+  print("1.Add")
+  print("2.Subtract")
+  print("3.Devide")
+  print("4.Multiply")
+
+  Operator = int(input())
+
+  if Operator == 1:
+    num1 = float(input("Number 1: "))
+    num2 = float(input("Number 2: "))
+    answer = num1 + num2
+    print(f"Answer: {answer}")
+
+    rerun = input("\nDo you want to run this again? [y/n]\n")
+
+    if rerun == "y":
+      code()
+
+  if Operator == 2:
+    num1 = float(input("Number 1: "))
+    num2 = float(input("Number 2: "))
+    answer = num1 - num2
+    print(f"Answer: {answer}")
+
+    rerun = input("\nDo you want to run this again? [y/n]\n")
+
+    if rerun == "y":
+      code()
+
+  if Operator == 3:
+    num1 = float(input("Number 1: "))
+    num2 = float(input("Number 2: "))
+    answer = num1 / num2
+    print(f"Answer: {answer}")
+
+    rerun = input("\nDo you want to run this again? [y/n]\n")
+
+    if rerun == "y":
+      code()
+
+  if Operator == 4:
+    num1 = float(input("Number 1: "))
+    num2 = float(input("Number 2: "))
+    answer = num1 * num2
+    print(f"Answer: {answer}")
+
+    rerun = input("\nDo you want to run this again? [y/n]\n")
+
+    if rerun == "y":
+      code()
+
+code()
